5ba6b0ac7d3b36246249eb4b7713b0e5e060d84b
Juan Cruz Viotti <jviotti@openmailbox.org>
2016-09-29 12:43:17 -0400

    Implement inquirer .transpileQuestion()
    Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

---
+++ tests/visuals/cli/integrations/inquirer.spec.js
@@ -0,0 +1,268 @@
+/*
+ * Copyright 2016 Resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const ava = require('ava');
+const inquirer = require('../../../../lib/visuals/cli/integrations/inquirer');
+
+ava.test('.transpileQuestion() should throw if type is not recognised', (test) => {
+  test.throws(() => {
+    inquirer.transpileQuestion({
+      title: 'Foo',
+      name: 'foo',
+      type: 'foo'
+    });
+  }, 'Unknown question type: foo');
+});
+
+ava.test('.transpileQuestion() should throw if question has no title', (test) => {
+  test.throws(() => {
+    inquirer.transpileQuestion({
+      name: 'foo',
+      type: 'text'
+    });
+  }, 'Invalid question title: undefined');
+});
+
+ava.test('.transpileQuestion() should throw if question has an invalid title', (test) => {
+  test.throws(() => {
+    inquirer.transpileQuestion({
+      title: [ 'foo', 'bar' ],
+      name: 'foo',
+      type: 'text'
+    });
+  }, 'Invalid question title: foo,bar');
+});
+
+ava.test('.transpileQuestion() should throw if question has no name', (test) => {
+  test.throws(() => {
+    inquirer.transpileQuestion({
+      title: 'Foo',
+      type: 'text'
+    });
+  }, 'Invalid question name: undefined');
+});
+
+ava.test('.transpileQuestion() should throw if question has an invalid name', (test) => {
+  test.throws(() => {
+    inquirer.transpileQuestion({
+      title: 'Foo',
+      name: [ 'foo', 'bar' ],
+      type: 'text'
+    });
+  }, 'Invalid question name: foo,bar');
+});
+
+ava.test('.transpileQuestion() it should transpile a basic text question', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Wifi SSID',
+    name: 'ssid',
+    type: 'text'
+  }), {
+    message: 'Wifi SSID',
+    name: 'ssid',
+    type: 'input'
+  });
+});
+
+ava.test('.transpileQuestion() it should transpile a basic password question', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Wifi Key',
+    name: 'key',
+    type: 'password'
+  }), {
+    message: 'Wifi Key',
+    name: 'key',
+    type: 'password'
+  });
+});
+
+ava.test('.transpileQuestion() it should transpile a basic number question', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Update Poll Interval',
+    name: 'updatePollInterval',
+    type: 'number'
+  }), {
+    message: 'Update Poll Interval',
+    name: 'updatePollInterval',
+    type: 'input'
+  });
+});
+
+ava.test('.transpileQuestion() it should transpile a basic editor question', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Welcome message',
+    name: 'welcome',
+    type: 'editor'
+  }), {
+    message: 'Welcome message',
+    name: 'welcome',
+    type: 'editor'
+  });
+});
+
+ava.test('.transpileQuestion() it should allow a default text value', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Wifi SSID',
+    name: 'ssid',
+    type: 'text',
+    default: 'mynetwork'
+  }), {
+    message: 'Wifi SSID',
+    name: 'ssid',
+    type: 'input',
+    default: 'mynetwork'
+  });
+});
+
+ava.test('.transpileQuestion() it should allow a default password value', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Wifi Key',
+    name: 'key',
+    type: 'password',
+    default: 'secret'
+  }), {
+    message: 'Wifi Key',
+    name: 'key',
+    type: 'password',
+    default: 'secret'
+  });
+});
+
+ava.test('.transpileQuestion() it should allow a default number value', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Update Poll Interval',
+    name: 'updatePollInterval',
+    type: 'number',
+    default: 60000
+  }), {
+    message: 'Update Poll Interval',
+    name: 'updatePollInterval',
+    type: 'input',
+
+    // Inquirer doesn't support a number input type, so this
+    // test ensures the number is converted into a string.
+    default: '60000'
+
+  });
+});
+
+ava.test('.transpileQuestion() it should allow a default editor value', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Welcome message',
+    name: 'welcome',
+    type: 'editor',
+    default: 'Welcome!'
+  }), {
+    message: 'Welcome message',
+    name: 'welcome',
+    type: 'editor',
+    default: 'Welcome!'
+  });
+});
+
+ava.test('.transpileQuestion() it should transpile a basic list question', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Network Type',
+    name: 'networkType',
+    type: 'list',
+    choices: [
+      {
+        name: 'Wifi',
+        value: 'wifi'
+      },
+      {
+        name: 'Ethernet',
+        value: 'ethernet'
+      }
+    ]
+  }), {
+    message: 'Network Type',
+    name: 'networkType',
+    type: 'list',
+    choices: [
+      {
+        name: 'Wifi',
+        value: 'wifi'
+      },
+      {
+        name: 'Ethernet',
+        value: 'ethernet'
+      }
+    ]
+  });
+});
+
+ava.test('.transpileQuestion() it should allow a default list value', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Network Type',
+    name: 'networkType',
+    type: 'list',
+    default: 'wifi',
+    choices: [
+      {
+        name: 'Wifi',
+        value: 'wifi'
+      },
+      {
+        name: 'Ethernet',
+        value: 'ethernet'
+      }
+    ]
+  }), {
+    message: 'Network Type',
+    name: 'networkType',
+    type: 'list',
+    default: 'wifi',
+    choices: [
+      {
+        name: 'Wifi',
+        value: 'wifi'
+      },
+      {
+        name: 'Ethernet',
+        value: 'ethernet'
+      }
+    ]
+  });
+});
+
+ava.test('.transpileQuestion() it should transpile a basic checkbox question', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Enable HDMI',
+    name: 'hdmi',
+    type: 'checkbox'
+  }), {
+    message: 'Enable HDMI',
+    name: 'hdmi',
+    type: 'checkbox'
+  });
+});
+
+ava.test('.transpileQuestion() it should allow a default checkbox value', (test) => {
+  test.deepEqual(inquirer.transpileQuestion({
+    title: 'Enable HDMI',
+    name: 'hdmi',
+    type: 'checkbox',
+    default: false
+  }), {
+    message: 'Enable HDMI',
+    name: 'hdmi',
+    type: 'checkbox',
+    default: false
+  });
+});
---
+++ tests/visuals/cli/integrations/when.spec.js
@@ -0,0 +1,286 @@
+/*
+ * Copyright 2016 Resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const ava = require('ava');
+const _ = require('lodash');
+const inquirer = require('../../../../lib/visuals/cli/integrations/inquirer');
+
+_.attempt(() => {
+  const question = inquirer.transpileQuestion({
+    title: 'Wifi SSID',
+    name: 'ssid',
+    type: 'text',
+    when: {
+      networkType: 'wifi'
+    }
+  });
+
+  ava.test('(string property) should return false for an empty object', (test) => {
+    test.false(question.when({}));
+  });
+
+  ava.test('(string property) should return true if it matches', (test) => {
+    test.true(question.when({
+      networkType: 'wifi'
+    }));
+  });
+
+  ava.test('(string property) should return true if is a subset', (test) => {
+    test.true(question.when({
+      networkType: 'wifi',
+      wifiKey: 'foo bar'
+    }));
+  });
+
+  ava.test('(string property) should return false if is not a subset', (test) => {
+    test.false(question.when({
+      wifiKey: 'foo bar'
+    }));
+  });
+
+  ava.test('(string property) should return true if it does not match', (test) => {
+    test.false(question.when({
+      networkType: 'ethernet'
+    }));
+  });
+});
+
+_.attempt(() => {
+  const question = inquirer.transpileQuestion({
+    title: 'HDMI',
+    name: 'hdmi',
+    type: 'checkbox',
+    when: {
+      enableScreen: true
+    }
+  });
+
+  ava.test('(boolean property) should return false for an empty object', (test) => {
+    test.false(question.when({}));
+  });
+
+  ava.test('(boolean property) should return true if true', (test) => {
+    test.true(question.when({
+      enableScreen: true
+    }));
+  });
+
+  ava.test('(boolean property) should return false if false', (test) => {
+    test.false(question.when({
+      enableScreen: false
+    }));
+  });
+
+  ava.test('(boolean property) should return false if 0', (test) => {
+    test.false(question.when({
+      enableScreen: 0
+    }));
+  });
+
+  ava.test('(boolean property) should return false if 1', (test) => {
+    test.false(question.when({
+      enableScreen: 0
+    }));
+  });
+
+  ava.test('(boolean property) should return false if undefined', (test) => {
+    test.false(question.when({
+      enableScreen: undefined
+    }));
+  });
+
+  ava.test('(boolean property) should return false if null', (test) => {
+    test.false(question.when({
+      enableScreen: undefined
+    }));
+  });
+});
+
+_.attempt(() => {
+  const question = inquirer.transpileQuestion({
+    title: 'HDMI',
+    name: 'hdmi',
+    type: 'checkbox',
+    when: {
+      capabilities: [
+        'screen',
+        'interactive',
+        'touch'
+      ]
+    }
+  });
+
+  ava.test('(string array property) should return false if empty object', (test) => {
+    test.false(question.when({}));
+  });
+
+  ava.test('(string array property) should return true if it matches', (test) => {
+    test.true(question.when({
+      capabilities: [
+        'screen',
+        'interactive',
+        'touch'
+      ]
+    }));
+  });
+
+  ava.test('(string array property) should return false if subset', (test) => {
+    test.false(question.when({
+      capabilities: [
+        'screen',
+        'touch'
+      ]
+    }));
+  });
+
+  ava.test('(string array property) should return true if superset', (test) => {
+    test.true(question.when({
+      capabilities: [
+        'screen',
+        'interactive',
+        'touch',
+        'blink',
+        'battery'
+      ]
+    }));
+  });
+
+});
+
+_.attempt(() => {
+  const question = inquirer.transpileQuestion({
+    title: 'HDMI',
+    name: 'hdmi',
+    type: 'checkbox',
+    when: {
+      capabilities: [
+        {
+          name: 'screen'
+        },
+        {
+          name: 'interactive'
+        },
+        {
+          name: 'touch'
+        }
+      ]
+    }
+  });
+
+  ava.test('(object array property) should return false if empty object', (test) => {
+    test.false(question.when({}));
+  });
+
+  ava.test('(object array property) should return true if it matches', (test) => {
+    test.true(question.when({
+      capabilities: [
+        {
+          name: 'screen'
+        },
+        {
+          name: 'interactive'
+        },
+        {
+          name: 'touch'
+        }
+      ]
+    }));
+  });
+
+  ava.test('(object array property) should return false if subset', (test) => {
+    test.false(question.when({
+      capabilities: [
+        {
+          name: 'screen'
+        },
+        {
+          name: 'interactive'
+        }
+      ]
+    }));
+  });
+
+  ava.test('(object array property) should return true if superset', (test) => {
+    test.true(question.when({
+      capabilities: [
+        {
+          name: 'screen'
+        },
+        {
+          name: 'interactive'
+        },
+        {
+          name: 'touch'
+        },
+        {
+          name: 'blink'
+        },
+        {
+          name: 'battery'
+        }
+      ]
+    }));
+  });
+
+});
+
+_.attempt(() => {
+  const question = inquirer.transpileQuestion({
+    title: 'Wifi SSID',
+    name: 'ssid',
+    type: 'text',
+    when: {
+      enableWifi: true,
+      network: true,
+      networkType: 'wifi'
+    }
+  });
+
+  ava.test('(multiple string property) should return true if it matches', (test) => {
+    test.true(question.when({
+      enableWifi: true,
+      network: true,
+      networkType: 'wifi'
+    }));
+  });
+
+  ava.test('(multiple string property) should return true if is a subset', (test) => {
+    test.true(question.when({
+      enableWifi: true,
+      network: true,
+      networkType: 'wifi',
+      enableFoo: false,
+      wifiKey: 'secret'
+    }));
+  });
+
+  ava.test('(multiple string property) should return false if is not a subset', (test) => {
+    test.false(question.when({
+      enableWifi: true,
+      networkType: 'wifi'
+    }));
+  });
+
+  ava.test('(multiple string property) should return true if it does not match', (test) => {
+    test.false(question.when({
+      enableWifi: true,
+      network: false,
+      networkType: 'wifi'
+    }));
+  });
+});

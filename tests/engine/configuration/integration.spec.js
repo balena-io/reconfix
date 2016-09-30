6b031dcb71ed5c4c74c3bb9e426e1b38d08bcc6d
Juan Cruz Viotti <jviotti@openmailbox.org>
2016-09-30 15:36:04 -0400

    Implement configuration file
    Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

---
+++ tests/engine/configuration/integration.spec.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 Resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const ava = require('ava');
+const path = require('path');
+const configuration = require('../../../lib/engine/configuration');
+
+const testFixture = (name) => {
+  const fixturePath = path.join(__dirname, 'fixtures', name);
+  const files = {
+    dry: require(path.join(fixturePath, 'dry.json')),
+    wet: require(path.join(fixturePath, 'wet.json')),
+    schema: require(path.join(fixturePath, 'schema.json'))
+  };
+
+  ava.test(`(${name}) should generate configuration`, (test) => {
+    test.deepEqual(configuration.generate(files.schema, files.dry), files.wet);
+  });
+
+  ava.test(`(${name}) should extract settings`, (test) => {
+    test.deepEqual(configuration.extract(files.schema, files.wet), files.dry);
+  });
+};
+
+testFixture('resinos-v1');

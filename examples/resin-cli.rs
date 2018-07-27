e691a69e4c87fcd2b21c365a190c8bfb8dbc77a6
Zrzka <robert@resin.io>
2018-07-27 17:30:37 +0200

    example: Add clap to resin-cli
    Use clap to parse command line arguments.
    
    Change-type: minor
    Signed-off-by: Zrzka <robert@resin.io>

--- examples/resin-cli.rs
+++ examples/resin-cli.rs
@@ -3,19 +3,26 @@
 #![warn(rust_2018_compatibility)]
 #![warn(rust_2018_idioms)]
 
-use std::env;
 use std::fs;
 
+use clap::{App, Arg};
+
 fn main() {
-    let args = env::args().collect::<Vec<_>>();
-    let command = args.get(1).expect("command required");
-    let mut reconfix = ::reconfix::Reconfix::new();
-    match command.as_ref() {
-        "test" => {
-            let path = args.get(2).expect("file arg required");
-            let file = fs::File::open(path).expect("file not found");
-            reconfix.load_schema(file).expect("unable to load schema");
-        },
-        _ => unimplemented!(),
-    }
+    let matches = App::new("resin-cli")
+        .arg(
+            Arg::with_name("SCHEMA")
+                .required(true)
+                .takes_value(true)
+                .short("s")
+                .long("schema")
+                .help("Resin JSON schema path"),
+        )
+        .get_matches();
+
+    let schema_path = matches.value_of("SCHEMA").unwrap();
+    let schema_file = fs::File::open(schema_path).expect("Schema file not found");
+
+    let mut rfx = reconfix::Reconfix::new();
+
+    println!("{:?}", rfx.load_schema(schema_file));
 }

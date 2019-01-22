96a67278e0d43b8794d3d68d14f735e333725a67
Robert Vojta <robert@balena.io>
2019-01-22 13:07:29 +0100

    Replace regex with std::net::IpAddr parser
    Signed-off-by: Robert Vojta <robert@balena.io>

--- src/validator/types/ip.rs
+++ src/validator/types/ip.rs
@@ -1,25 +1,42 @@
-use lazy_static::lazy_static;
-use regex::Regex;
+use std::net::IpAddr;
+
 use serde_json::Value;
 
 use super::{
     super::{scope::ScopedSchema, state::ValidationState},
-    regex::validate_as_string_with_regex,
+    string::validate_as_string,
 };
 
-lazy_static! {
-    // ajv v6.7.0 compatible
-    // https://github.com/epoberezkin/ajv/blob/v6.7.0/lib/compile/formats.js
-    static ref IPV4_REGEX: Regex =
-        Regex::new(r"^(?i)(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)$").unwrap();
-    static ref IPV6_REGEX: Regex =
-        Regex::new(r"^(?i)\s*(?:(?:(?:[0-9a-f]{1,4}:){7}(?:[0-9a-f]{1,4}|:))|(?:(?:[0-9a-f]{1,4}:){6}(?::[0-9a-f]{1,4}|(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(?:(?:[0-9a-f]{1,4}:){5}(?:(?:(?::[0-9a-f]{1,4}){1,2})|:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(?:(?:[0-9a-f]{1,4}:){4}(?:(?:(?::[0-9a-f]{1,4}){1,3})|(?:(?::[0-9a-f]{1,4})?:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?:(?:[0-9a-f]{1,4}:){3}(?:(?:(?::[0-9a-f]{1,4}){1,4})|(?:(?::[0-9a-f]{1,4}){0,2}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?:(?:[0-9a-f]{1,4}:){2}(?:(?:(?::[0-9a-f]{1,4}){1,5})|(?:(?::[0-9a-f]{1,4}){0,3}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?:(?:[0-9a-f]{1,4}:){1}(?:(?:(?::[0-9a-f]{1,4}){1,6})|(?:(?::[0-9a-f]{1,4}){0,4}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?::(?:(?:(?::[0-9a-f]{1,4}){1,7})|(?:(?::[0-9a-f]{1,4}){0,5}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(?:%.+)?\s*$").unwrap();
-}
-
 pub fn validate_as_ipv4(scope: &ScopedSchema, data: &Value) -> ValidationState {
-    validate_as_string_with_regex(scope, data, &IPV4_REGEX)
+    let mut state = validate_as_string(scope, data);
+    if !state.is_valid() {
+        return state;
+    }
+
+    let ip: Result<IpAddr, _> = data.as_str().expect("invalid validate_as_string").parse();
+
+    match ip {
+        Ok(x) if !x.is_ipv4() => state.push_error(scope.invalid_error("not an IPv4")),
+        Err(_) => state.push_error(scope.invalid_error("not an IP address")),
+        _ => {}
+    };
+
+    state
 }
 
 pub fn validate_as_ipv6(scope: &ScopedSchema, data: &Value) -> ValidationState {
-    validate_as_string_with_regex(scope, data, &IPV6_REGEX)
+    let mut state = validate_as_string(scope, data);
+    if !state.is_valid() {
+        return state;
+    }
+
+    let ip: Result<IpAddr, _> = data.as_str().expect("invalid validate_as_string").parse();
+
+    match ip {
+        Ok(x) if !x.is_ipv6() => state.push_error(scope.invalid_error("not an IPv4")),
+        Err(_) => state.push_error(scope.invalid_error("not an IP address")),
+        _ => {}
+    };
+
+    state
 }
